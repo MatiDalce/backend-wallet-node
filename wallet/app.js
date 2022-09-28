5acc8ca6c6af9803d2bd22772d6e3fc3e432efa7
MATIAS DALCEGGIO <matiasdalceggio@gmail.com>
2022-09-28 19:04:34 -0300

    CORS TO REACT

--- wallet/app.js
+++ wallet/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
+app.use(cors())
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 
 
 // catch 404 and forward to error handler
